65be8e2fdc1dc7b876b4b14ca6ca1ab21835b9a5
Eric <eyang@mie.utoronto.ca>
2020-05-20 13:36:30 -0400

    change the SIG so that margins are added properly

--- synimage/synimagegen.py
+++ synimage/synimagegen.py
@@ -185,8 +185,8 @@ def create_synimage_parameters(input_data, x_bound, y_bound, image_size, path='N
     margin = ((x_bound[1] - x_bound[0]) * 0.2, (y_bound[1] - y_bound[0]) * 0.2)
     num_of_par = int(image_size[0] * image_size[1] * den)
     num_of_lost_pairs = num_of_par * (per_loss_pairs / 100)
-    x_1 = np.random.uniform(x_bound[0] - margin[0], x_bound[0] + margin, num_of_par)
-    y_1 = np.random.uniform(y_bound[0] - margin[1], y_bound[1] + margin, num_of_par)
+    x_1 = np.random.uniform(x_bound[0] - margin[0], x_bound[0] + margin[0], num_of_par)
+    y_1 = np.random.uniform(y_bound[0] - margin[1], y_bound[1] + margin[1], num_of_par)
     par_diam1 = np.random.normal(par_diam_mean, par_diam_std, num_of_par)
     particleCenters = np.random.uniform(size=num_of_par) - 0.5
     par_int1 = np.exp(-particleCenters ** 2 / (2 * par_int_std ** 2))
