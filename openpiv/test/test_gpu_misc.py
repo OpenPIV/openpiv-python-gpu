752fc5a121d47927eab63647811110715a76c172
ericyang <ericyang125@gmail.com>
2022-12-05 19:35:36 -0700

    Add functions to generate array using center/width arguments.

--- openpiv/test/test_gpu_misc.py
+++ openpiv/test/test_gpu_misc.py
@@ -36,12 +36,37 @@ def generate_array_pair(shape, magnitude=1.0, offset=0.0, d_type=DTYPE_f, seed=0
     return f, f_d
 
 
+def generate_np_array1(shape, center=0.0, width=1.0, d_type=DTYPE_f, seed=0):
+    """Returns ndarray with pseudo-random values."""
+    np.random.seed(seed)
+    f = ((np.random.random(shape) * 2 - 1) * width + center).astype(d_type)
+
+    return f
+
+
+def generate_gpu_array1(shape, center=0.0, width=1.0, d_type=DTYPE_f, seed=0):
+    """Returns ndarray with pseudo-random values."""
+    f = generate_np_array1(shape, center=center, width=width, d_type=d_type, seed=seed)
+    f_d = gpuarray.to_gpu(f)
+
+    return f_d
+
+
+def generate_array_pair1(shape, center=0.0, width=1.0, d_type=DTYPE_f, seed=0):
+    """Returns a pair of numpy and gpu arrays with identical pseudo-random values."""
+    f = generate_np_array1(shape, center=center, width=width, d_type=d_type, seed=seed)
+    f_d = gpuarray.to_gpu(f)
+
+    return f, f_d
+
+
 # UNIT TESTS
-def test_gpu_mask():
+@pytest.mark.parametrize('d_type', [DTYPE_i, DTYPE_f])
+def test_gpu_mask(d_type):
     shape = (16, 16)
 
-    f, f_d = generate_array_pair(shape, magnitude=2, offset=-1, d_type=DTYPE_f)
-    mask, mask_d = generate_array_pair(shape, magnitude=2, d_type=DTYPE_i)
+    f, f_d = generate_array_pair1(shape, center=1, d_type=d_type)
+    mask, mask_d = generate_array_pair1(shape, center=1, d_type=DTYPE_i)
 
     f_masked = f * (1 - mask)
     f_masked_gpu = gpu_misc.gpu_mask(f_d, mask_d).get()
